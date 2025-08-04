7b094a74608112c31c212db2775f7820dce1859c
Andy Fdez <andyfg87@gmail.com>
2025-08-04 15:49:04 -0400

    Mostrar las Impresoras de un Cliente en la vista detalles #18

--- CapoteSolution.Web/Controllers/CustomersController.cs
+++ CapoteSolution.Web/Controllers/CustomersController.cs
@@ -3,24 +3,40 @@ using CapoteSolution.Web.Interface;
 using CapoteSolution.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 
 namespace CapoteSolution.Web.Controllers
 {
     [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Technician)}")]
     public class CustomersController : AbstractEntityManagementController<Customer, System.Guid, CustomerInputVM, CustomerDisplayVM>
-    {
-        public CustomersController(IEntityRepository<Customer, Guid> repository, IStringLocalizer<CustomersController> localizer, ILogger<CustomersController> logger) : base(repository, localizer, logger)
+    {    
+        private readonly IEntityRepository<Copier, string> _copierRepo;
+
+        public CustomersController(IEntityRepository<Customer, Guid> repository, IEntityRepository<Copier, string> copierRepo, IStringLocalizer<CustomersController> localizer, ILogger<CustomersController> logger) : base(repository, localizer, logger)
         {
+            this._copierRepo = copierRepo;
         }
 
         public override async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10)
         {
-            var customers = _repository.GetAllWithNestedInclude(nameof(Copier));
+            var customers = _repository.GetAllWithNestedInclude("Copiers");
 
             var paginatedData = await GetPaginatedData(customers.Result, pageNumber, pageSize);
 
             return View(paginatedData);
         }
+
+        public override async Task<IActionResult> Details(Guid key)
+        {
+            var entity = await _repository.GetAllWithNestedInclude("Copiers").Result.FirstAsync(c => c.Id == key);
+
+            var viewModel = new CustomerDisplayVM();
+            viewModel.Import(entity);
+
+            ViewBag.Copiers = entity.Copiers;
+
+            return View(viewModel);
+        }        
     }
 }
